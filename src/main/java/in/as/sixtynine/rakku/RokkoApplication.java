c9d928f2b425180d60bacf0d003ac7095470a0f2
Sanjay Das - s0d062y <sanjay.das@walmart.com>
2022-01-03 23:56:41 +0530

    Done -1

--- src/main/java/in/as/sixtynine/rakku/RokkoApplication.java
+++ src/main/java/in/as/sixtynine/rakku/RokkoApplication.java
@@ -1,14 +1,17 @@
 package in.as.sixtynine.rakku;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class RokkoApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting app now....");
 		SpringApplication.run(RokkoApplication.class, args);
 	}
 
